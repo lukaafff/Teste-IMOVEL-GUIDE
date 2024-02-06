eac06fc5a00e6227374a8b8fd0fc9d94349ac727
lukaafff <luizagarcia2405@gmail.com>
2024-02-05 22:05:48 -0300

    configuração banco de dados

---
+++ TestedeProgramaçãoPrático/database/migrations/2024_02_06_010129_create_model_corretors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('corretor', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 20)->nullable(false);
+            $table->string('cpf', 11)->unique()->nullable(false);
+            $table->string('creci', 8)->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('corretor');
+    }
+};
